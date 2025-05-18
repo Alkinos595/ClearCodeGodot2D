8fa42e63700e259c0caabb2a325e3a8760ccc248
Alkinos <foinquinosalberto@gmail.com>
2025-05-18 23:40:38 +0200

    Update 18-may

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,1 +1,26 @@
 extends Node2D
+
+var laser_scene: PackedScene = preload("res://scenes/projectiles/laser.tscn")
+var grenade_scene: PackedScene = preload("res://scenes/projectiles/grenade.tscn")
+
+func _on_gate_player_entered_gate(body) -> void:
+	print("Player has entered gate")
+	print(body)
+
+func _on_player_laser(pos, direction) -> void:
+	var laser = laser_scene.instantiate() as Area2D
+	# 1. update the laser position
+	laser.position = pos
+	laser.rotation_degrees = rad_to_deg(direction.angle()) + 90
+	laser.direction = direction
+	# 2. we have to move the laser
+	#This change is in laser.gd
+	
+	# 3. I want to add the laser instance to a Node2D
+	$Projectiles.add_child(laser)
+
+func _on_player_grenade(pos, direction) -> void:
+	var grenade = grenade_scene.instantiate() as RigidBody2D
+	grenade.position = pos
+	grenade.linear_velocity = direction * grenade.SPEED
+	$Projectiles.add_child(grenade)
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,5 +1,10 @@
 extends CharacterBody2D
 
+signal laser(pos: Vector2, direction:Vector2)
+signal grenade (pos: Vector2, direction: Vector2)
+
+var can_laser: bool = true
+var can_grenade: bool = true
 
 func _process(_delta: float) -> void:
 	# input
@@ -7,8 +12,30 @@ func _process(_delta: float) -> void:
 	velocity = direction * 500
 	move_and_slide()
 	
+	# rotate
+	look_at(get_global_mouse_position())
+	
 	# laser shooting input
-	if Input.is_action_pressed("primary action"):
-		print("shoot laser")
-	if Input.is_action_pressed("secondary action"):
-		print("shoot grenade")
+	var player_direction = (get_global_mouse_position() - position).normalized()
+	if Input.is_action_pressed("primary action") and can_laser:
+		# randomly selected a market2D for the laser start
+		var laser_markers = $LaserStartPositions.get_children()
+		#var selected_laser = laser_markers[randi() % laser_markers.size()]
+		var selected_laser = laser_markers[randi_range(0, laser_markers.size() - 1)]
+		can_laser = false
+		$LaserTimer.start()
+		# emit the position we selected
+		laser.emit(selected_laser.global_position, player_direction)
+		
+	if Input.is_action_pressed("secondary action") and can_grenade:
+		var grenade_markers = $GrenadeStartPositions.get_children()
+		var selected_grenade = grenade_markers[randi_range(0, grenade_markers.size() - 1)]
+		can_grenade = false
+		$GrenadeTimer.start()
+		grenade.emit(selected_grenade.global_position, player_direction)
+		
+func _on_laser_timer_timeout() -> void:
+	can_laser = true
+
+func _on_grenade_timer_timeout() -> void:
+	can_grenade = true
